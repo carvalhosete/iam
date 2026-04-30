62ebecae1bc794e2a32b7fa0ca5af83f2cfdf35d
carvalhosete <lcarvalho7@outlook.com>
2026-04-30 02:30:12 -0300

    refactor: extrai consumidores e serviço de email para microserviço independente

---
+++ src/main/java/br/com/project/iam/dto/NotificacaoBoasVindasDTO.java
@@ -0,0 +1,6 @@
+package br.com.project.iam.dto;
+
+public record NotificacaoBoasVindasDTO(
+        String email,
+        String nome
+) {}
--- src/main/java/br/com/project/iam/service/UsuarioService.java
+++ src/main/java/br/com/project/iam/service/UsuarioService.java
@@ -1,9 +1,11 @@
 package br.com.project.iam.service;
 
 import br.com.project.iam.domain.Usuario;
+import br.com.project.iam.dto.NotificacaoBoasVindasDTO;
 import br.com.project.iam.dto.UsuarioRequestDTO;
 import br.com.project.iam.dto.UsuarioResponseDTO;
 import br.com.project.iam.repository.UsuarioRepository;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
+    private final SqsTemplate sqsTemplate;
 
     public UsuarioResponseDTO criarUsuario(UsuarioRequestDTO usuarioDTO){
 
@@ -23,6 +26,16 @@ public class UsuarioService {
         usuarioEntity.setSenha(passwordEncoder.encode(usuarioDTO.senha()));
 
         Usuario usuarioSalvo = usuarioRepository.save(usuarioEntity);
+
+        try{
+            NotificacaoBoasVindasDTO payload = new NotificacaoBoasVindasDTO(
+                    usuarioSalvo.getEmail(),
+                    usuarioSalvo.getNome()
+            );
+            sqsTemplate.send("fila-notificacao-boas-vindas", payload);
+        } catch (Exception e){
+            System.out.println("Erro ao notificar fila de boas-vindas " + e.getMessage());
+        }
 
         return UsuarioResponseDTO.fromEntity(usuarioSalvo);
     }
